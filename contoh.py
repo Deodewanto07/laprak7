d8e120444d5bb3c140c0ecf419e813bb1d85bfe1
Deodewanto07 <143709679+Deodewanto07@users.noreply.github.com>
2024-03-26 20:13:17 +0700

    Add files via upload

---
+++ contoh.py
@@ -0,0 +1,9 @@
+buah_1 = "appel"
+buah_2 = "jeruk"
+buah_3 = "semangka "
+buah_4 = "buah" + 'naga'
+
+print(buah_4)
+print(buah_1[1])
+print(buah_2[2])
+print(buah_3[3])
---
+++ contoh2.py
@@ -0,0 +1,7 @@
+kalimat = "Nama saya bambang"
+print(len(kalimat))
+terakhir = kalimat[len(kalimat)-1]
+print(terakhir)
+
+alternatif_terakhir = kalimat[-1]
+print(alternatif_terakhir)
